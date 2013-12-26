537677f1382fff75337153de0cca58b0017e0a51
3of8 <eberlm@in.tum.de>
2013-12-26 18:21:23 +0100

    Fixed compile script

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 mkdir -p bin
 cd src
 ghc -o ../bin/paste_raw.cgi LightPaste.hs && strip -p --strip-unneeded --remove-section=.comment -o ../bin/paste.cgi ../bin/paste_raw.cgi
-#sudo cp paste.cgi /var/www/paste/
 cd ..
+sudo cp bin/paste.cgi /var/www/paste/
